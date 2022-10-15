dfd019385d2e7bc0f0a1bdffbcfb95c84f280f42
MelonGO <494216469@qq.com>
2022-10-15 16:31:44 +0800

    04:31 15-Oct

--- flask/server.py
+++ flask/server.py
@@ -54,6 +54,10 @@ def proxy():
 def cicd():
     return render_template('cicd.html')
 
+@app.route('/imgrating')
+def imgrating():
+    return render_template('imgrating.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
